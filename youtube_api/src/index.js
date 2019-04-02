2225bd1f0aceae88a53562393798720fea67f6a8
uvshrivass <uvshrivas@gmail.com>
2019-04-02 23:32:49 +0530

    React Youtube API

--- youtube_api/src/index.js
+++ youtube_api/src/index.js
@@ -5,7 +5,7 @@ import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
 import YTSearch from "youtube-api-search";
-const API_KEY = "AIzaSyAB_CN30ouaEgNLLx4FgVhOz8c1wBjJedk";
+const API_KEY = "Please provide your own API key";
 
 
 
