9e76a7c6131d66d7555a9ce98e256d9ef501519f
uvshrivass <uvshrivas@gmail.com>
2019-04-02 23:31:23 +0530

    React Youtube API

--- youtube_api/src/index.js
+++ youtube_api/src/index.js
@@ -1,46 +1,54 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import Searchbar from './components/Searchbar';
-import VideoList from "./components/VideoList.js";
+import _ from "lodash";
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 import YTSearch from "youtube-api-search";
-const API_KEY = "Please provide your own API key";
+const API_KEY = "AIzaSyAB_CN30ouaEgNLLx4FgVhOz8c1wBjJedk";
 
 
 
-// YTSearch({key: API_KEY, term:"elonmusk"},function(data){
-//     var data = data;
-//     console.log(data);
-//     console.log(data[0].id.videoId);
-// })
+//Creating Component
+class App extends Component{
 
-
-
-class App extends Component {
-
-
-    constructor(props){
-        super(props);
-        this.state = {
-            videos : []
-        };
-
-    YTSearch({key: API_KEY, term:"elonmusk"},(videos)=>{
-         this.setState({videos})
-        })
-      }
-
-  render() {
-      
-    return (
-      <div>
-          
-          <Searchbar />
-          <VideoList videos={this.state.videos}>
-		  </VideoList>
-         
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      videos : [],
+      selectedVideo : null
+    };
+    
+    this.videoSearch("elon musk tesla")
   }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term:term},(videos)=>{
+      this.setState({
+        videos: videos,
+        selectedVideo : videos[0]
+       })
+     })  
+  }
+
+ render(){
+
+  const videoSearch = _.debounce((term)=>{
+    this.videoSearch(term)
+  }, 50000);
+
+   return(
+     <div>
+	<br />
+       <SearchBar onSearchTermChange={term => this.videoSearch(term)}/> 
+	<br /><br />
+       <VideoDetail video={this.state.selectedVideo}/>
+       <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+     </div>
+   )
+ }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App />,document.querySelector(".container"))
+//Rendering Component
